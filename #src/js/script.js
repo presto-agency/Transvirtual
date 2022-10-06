d813921f713e4e5f16573500fbebe937d97944a2
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-10-06 16:29:24 +0300

    feat: add menu mob. -2

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,6 +2,12 @@ window.addEventListener('DOMContentLoaded', function () {
   if (window.screen.width < 1200) {
     headerActive()
   }
+  if (window.screen.width >= 1200) {
+    scrollPage()
+  }
+  if (window.screen.width < 1200) {
+    showSubmenu(document.querySelectorAll('.submenu'))
+  }
 })
 
 const headerActive = () => {
@@ -25,29 +31,51 @@ const headerActive = () => {
   }
 }
 
-// const bg = () => {
-//     document.querySelectorAll(".ibg").forEach(el => {
-//         if (el.querySelector('img')) {
-//             el.style.backgroundImage = 'url(' + el.querySelector('img').getAttribute('src') + ')';
-//             el.querySelector('img').style.display = 'none';
-//         }
-//     });
-// }
+const scrollPage = () => {
+    let scrollPosition;
+    const header = document.querySelector('.header');
+    const scrollChange = 1;
 
-// const scrollPage = () => {
-//     let scrollPosition;
-//     const header = document.querySelector('.header');
-//     const scrollChange = 1;
+    const addClass = () => header.classList.add("hide")
+    const removeClass = () => header.classList.remove("hide")
 
-//     const addClass = () => header.classList.add("hide")
-//     const removeClass = () => header.classList.remove("hide")
+    window.addEventListener('scroll', function () {
+        scrollPosition = window.scrollY;
+        scrollPosition >= scrollChange?addClass():removeClass();
+    })
+}
 
-//     window.addEventListener('scroll', function () {
-//         scrollPosition = window.scrollY;
-//         if (scrollPosition >= scrollChange) { addClass() }
-//         else { removeClass() }
-//     })
-// }
+const showSubmenu = (itemsLinks) => {
+
+  function setHeight(itemsLinks) {
+    if (itemsLinks.length > 0) {
+      itemsLinks.forEach(item => {
+        let itemLink = item.querySelector('a');
+        if (!!itemLink) {
+          itemLink.onclick = (e) => {
+            e.preventDefault()
+            let itemsMenu = item.querySelectorAll('.dropdown-menu>li');
+            let submenuHeight = getHeight(itemsMenu)
+            checkHeight(itemsMenu[0], submenuHeight)
+            itemsMenu[0].parentNode.parentNode.classList.toggle('open')
+          }
+        }
+      })
+    }
+  }
+
+  function getHeight(items) {
+    let subHeight = Array.from(items).reduce((sum, current) =>
+      sum + current.offsetHeight, 0);
+    return subHeight
+  }
+
+  function checkHeight(item, height) {
+    item.parentNode.style.maxHeight = item.parentNode.style.maxHeight === height + 'px' ? '0' : height + 'px';
+  }
+
+  setHeight(itemsLinks)
+}
 
 
 
