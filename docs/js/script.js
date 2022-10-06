bec6a0a4170fd45f9347960e392f782b4347ad6d
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-10-06 15:40:37 +0300

    feat: add menu mob. -1

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
-
+  if (window.screen.width < 1200) {
+    headerActive()
+  }
 })
+
+const headerActive = () => {
+  const header = document.querySelector('.header');
+  const body = document.querySelector('body');
+  if (!!header) {
+    const burger = header.querySelector('.header__content_burger');
+    burger.onclick = () => {
+      header.classList.contains('active') ? removeClass() : addClass();
+
+      function addClass() {
+        header.classList.add('active');
+        body.style.overflow = 'hidden';
+      }
+
+      function removeClass() {
+        header.classList.remove('active');
+        body.style.overflow = '';
+      }
+    }
+  }
+}
 
 // const bg = () => {
 //     document.querySelectorAll(".ibg").forEach(el => {
